ac9cb4c8d0ff969fd48967c881621790756f37ad
abidur-abid <abidur.abid40@gmail.com>
2024-07-15 10:17:37 +0600

    added font, favicon, title

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,8 +5,14 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        roboto: ['Roboto'],
+      },
+    },
   },
-  plugins: [],
+  plugins: [
+    require('daisyui'),
+  ],
 }
 
