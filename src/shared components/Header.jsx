e81150e7a56e01023eff7e9a7c159f54f2c60138
abidur-abid <abidur.abid40@gmail.com>
2024-07-15 11:38:06 +0600

    added header

--- src/shared components/Header.jsx
+++ src/shared components/Header.jsx
@@ -3,6 +3,7 @@ import React, { useState } from 'react';
 import { HiMenu } from "react-icons/hi";
 import { AiOutlineClose } from "react-icons/ai";
 import { GoSearch } from "react-icons/go";
+import Sidebar from './Sidebar';
 
 // Define the Header functional component
 const Header = () => {
@@ -15,20 +16,24 @@ const Header = () => {
     }
 
     return (
-        // Define the header element with a class of 'mobileHeader'
-        <header className='mobileHeader'> 
-            <div className="mobileHeaderContainer">
-                <div className="mobileHeaderRow">
+        // Define the header element with relative positioning and high z-index
+        <header className="relative z-10">
+            {/* Flex container for the header content */}
+            <div className='flex justify-between items-center p-4 '>
+                {/* Flex container for the menu button and title */}
+                <div className='flex justify-center items-center gap-5'>
                     {/* Button to toggle sidebar; displays a menu icon if sidebar is closed, or a close icon if sidebar is open */}
                     <button onClick={handleSidebar}>
                         {isSidebarOpen ? <AiOutlineClose /> : <HiMenu />}
                     </button>
-                    {/* Placeholder for header title */}
-                    <div>Telegram</div>
+                    {/* Title of the header */}
+                    <div className="ml-2">Telegram</div>
                 </div>
                 {/* Search icon */}
-                <div><GoSearch /></div>
+                <div className='searchIcon'><GoSearch /></div>
             </div>
+            {/* Sidebar component with its visibility controlled by isSidebarOpen state */}
+            <Sidebar isOpen={isSidebarOpen} onClose={() => setIsSidebarOpen(false)} />
         </header>
     );
 };
